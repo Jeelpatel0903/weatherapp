9495d9615d202ea785b17cf1c55d2294e00d7a43
jeelpatel0903 <pateljeel6466@gmail.com>
2024-02-22 14:03:48 +0530

    addd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weatherapp';
+  ApiKey: string = "d5f94928c506eb0b1ecad1837f96c1dd";
+  weatherdata!: any
+  error: boolean = false
+  data : boolean = false;
+
+
+  constructor(private api: HttpClient) { }
+  
+
+
+  searchfunction(data: any) {
+
+    const getdataapi = `https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${this.ApiKey}&units=metric`
+    this.api.get(getdataapi).subscribe(e => {
+
+      if (this.weatherdata) {
+        this.weatherdata = "";
+      }
+      this.weatherdata = e;
+      console.log(e);
+      this.error = false;
+      this.data = true;
+      
+    }, err => {
+      this.data = false;
+      this.error = true
+      console.log(err);
+
+    })
+
+  }
+
 }
